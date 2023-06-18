22b8201c024fab5f4bda3f7da2bba250d83cfa14
Aelto <Aelto@users.noreply.github.com>
2023-06-18 14:59:11 +0200

    use builder pattern for buildgen

--- src/game/buildgen/index.ts
+++ src/game/buildgen/index.ts
@@ -43,110 +43,163 @@ export function generateSkillset(
     return new Set();
   }
 
-  const rng = new Rng(
-    `${character_name.toLowerCase()}-${outpost.link}-${profession}`
-  );
-  const available_skills = skills
-    .get(profession)
-    .filter((skill) => available_skill_origins.has(skill.options.origin));
+  return new BuildGenerator(
+    character_name,
+    outpost,
+    profession,
+    options,
+    available_skill_origins
+  )
+    .withSelfHeals(1)
+    .withRegularSkills(19)
+    .withElites() // the count is calculated inside the generator
+    .withDisabledSkills(
+      getSkillPenaltyFromHenchmen(
+        options.henchmen_count,
+        options.is_primary_profession
+      )
+    )
+    .build();
+}
 
-  const subsets = {
-    regulars: available_skills.filter((s) => !s.options.is_elite),
-    selfheals: available_skills.filter(
-      (s) => !s.options.is_elite && s.options.is_self_heal
-    ),
-    elites: available_skills.filter((s) => s.options.is_elite),
+class BuildGenerator {
+  private rng: Rng;
+
+  private available_skills: Skill[];
+  private subsets: {
+    regulars: Skill[];
+    selfheals: Skill[];
+    elites: Skill[];
   };
 
-  const SKILLSET_NORMAL_SIZE = Math.min(20, subsets.regulars.length);
-  let skillset: Set<Skill> = new Set();
+  private skillset: Set<Skill> = new Set();
+  private disabled_skills: Set<Skill> = new Set();
+  constructor(
+    character_name: string,
+    outpost: Outpost,
+    profession: Profession,
+    options: BuildGenOptions,
+    available_skill_origins: Set<SkillOrigin>
+  ) {
+    this.rng = new Rng(
+      `${character_name.toLowerCase()}-${outpost.link}-${profession}`
+    );
 
-  // 1.
-  // start by adding one guaranted self-heal
-  let shortcircuit = 100;
-  if (subsets.selfheals.length > 0) {
-    while (skillset.size < 1 && shortcircuit--) {
-      const index = rng.nextRange(subsets.selfheals.length);
+    this.available_skills = skills
+      .get(profession)
+      .filter((skill) => available_skill_origins.has(skill.options.origin));
 
-      skillset.add(subsets.selfheals.at(index));
-    }
+    this.subsets = {
+      regulars: this.available_skills.filter((s) => !s.options.is_elite),
+      selfheals: this.available_skills.filter(
+        (s) => !s.options.is_elite && s.options.is_self_heal
+      ),
+      elites: this.available_skills.filter((s) => s.options.is_elite),
+    };
   }
 
-  // 2.
-  // add regular skills until it reaches SKILLSET_NORMAL_SIZE
-  shortcircuit = 100;
-  while (skillset.size < SKILLSET_NORMAL_SIZE && shortcircuit--) {
-    const skill_index = rng.nextRange(subsets.regulars.length);
-    const skill = subsets.regulars.at(skill_index);
+  public withSelfHeals(count: number): BuildGenerator {
+    if (this.subsets.selfheals.length > 0) {
+      let target_size = this.skillset.size + count;
+      let shortcircuit = 100;
 
-    if (skillset.has(skill)) {
-      continue;
+      while (this.skillset.size < target_size && shortcircuit--) {
+        const index = this.rng.nextRange(this.subsets.selfheals.length);
+
+        this.skillset.add(this.subsets.selfheals.at(index));
+      }
     }
 
-    skillset.add(skill);
+    return this;
   }
 
-  // 3.
-  // if primary profession, add a random amount of elites to the skillset
-  if (options.is_primary_profession && subsets.elites.length > 0) {
-    const elites_count = rng.nextRange(Math.min(subsets.elites.length, 6), 3);
+  public withRegularSkills(count: number): BuildGenerator {
+    let target_size = Math.min(
+      this.skillset.size + count,
+      this.subsets.regulars.length
+    );
+    let shortcircuit = 100;
 
-    shortcircuit = 100;
-    while (
-      skillset.size < SKILLSET_NORMAL_SIZE + elites_count &&
-      shortcircuit--
-    ) {
-      const skill_index = rng.nextRange(subsets.elites.length);
-      const skill = subsets.elites.at(skill_index);
+    while (this.skillset.size < target_size && shortcircuit--) {
+      const skill_index = this.rng.nextRange(this.subsets.regulars.length);
+      const skill = this.subsets.regulars.at(skill_index);
 
-      if (skillset.has(skill)) {
+      if (this.skillset.has(skill)) {
         continue;
       }
 
-      skillset.add(skill);
+      this.skillset.add(skill);
     }
+
+    return this;
   }
 
-  // 4.
-  // construct a list of disabled skills from the henchmen penalty
-  const penalty = getSkillPenaltyFromHenchmen(
-    options.henchmen_count,
-    options.is_primary_profession
-  );
+  public withElites(): BuildGenerator {
+    if (this.subsets.elites.length <= 0) {
+      return this;
+    }
 
-  const skills_to_disable = Array.from(skillset);
-  const disabled_skills = new Set();
-  for (let i = 0; i < penalty; i += 1) {
-    const skill_index = rng.nextRange(skills_to_disable.length);
-    const [skill] = skills_to_disable.splice(skill_index, 1);
+    const count = this.rng.nextRange(6, 3);
+    const elites_count = Math.min(this.subsets.elites.length, count);
+    const target_size = this.skillset.size + elites_count;
+    let shortcircuit = 100;
 
-    disabled_skills.add(skill);
+    while (this.skillset.size < target_size && shortcircuit--) {
+      const skill_index = this.rng.nextRange(this.subsets.elites.length);
+      const skill = this.subsets.elites.at(skill_index);
+
+      if (this.skillset.has(skill)) {
+        continue;
+      }
+
+      this.skillset.add(skill);
+    }
+
+    return this;
   }
 
-  // 4.1
-  // but ensure there is at least 1 available heal even after the penalty:
-  const skills_array = Array.from(skillset);
-  const enabled_skills = new Set(
-    Array.from(skillset).filter((skill) => !disabled_skills.has(skill))
-  );
-  const skillset_heals = subsets.selfheals.filter((s) => skillset.has(s));
-  const enabled_heals = new Set(
-    subsets.selfheals.filter((s) => enabled_skills.has(s))
-  );
+  public withDisabledSkills(penalty: number): BuildGenerator {
+    const skills_to_disable = Array.from(this.skillset);
 
-  // 4.1.1
-  // enable back a random heal:
-  if (enabled_heals.size < 1) {
-    const skill_index = rng.nextRange(skillset_heals.length);
-    const skill = skillset_heals[skill_index];
+    for (let i = 0; i < penalty; i += 1) {
+      const skill_index = this.rng.nextRange(skills_to_disable.length);
+      const [skill] = skills_to_disable.splice(skill_index, 1);
 
-    disabled_skills.delete(skill);
+      this.disabled_skills.add(skill);
+    }
+
+    // but ensure there is at least 1 available heal even after the penalty:
+    const enabled_skills = new Set(
+      Array.from(this.skillset).filter(
+        (skill) => !this.disabled_skills.has(skill)
+      )
+    );
+    const skillset_heals = this.subsets.selfheals.filter((s) =>
+      this.skillset.has(s)
+    );
+    const enabled_heals = new Set(
+      this.subsets.selfheals.filter((s) => enabled_skills.has(s))
+    );
+
+    // enable back a random heal:
+    if (enabled_heals.size < 1) {
+      const skill_index = this.rng.nextRange(skillset_heals.length);
+      const skill = skillset_heals[skill_index];
+
+      this.disabled_skills.delete(skill);
+    }
+
+    return this;
   }
 
-  return new Set(
-    skills_array.map((skill) => ({
-      ...skill,
-      disabled: disabled_skills.has(skill),
-    }))
-  );
+  public build() {
+    const skills_array = Array.from(this.skillset);
+
+    return new Set(
+      skills_array.map((skill) => ({
+        ...skill,
+        disabled: this.disabled_skills.has(skill),
+      }))
+    );
+  }
 }
