366f815d96cf89ae85da7b8b55c7389f3d14ad92
Aelto <Aelto@users.noreply.github.com>
2023-06-21 19:55:44 +0200

    documentation

--- src/localstorage/index.ts
+++ src/localstorage/index.ts
@@ -1,9 +1,16 @@
 import { hasAnyQueryParam } from "../history";
 
+/**
+ * returns whether stores are allowed to store their values into the storage
+ */
 export function canStore() {
   return !hasAnyQueryParam();
 }
 
+/**
+ * returns whether the current tab is in preview mode, meaning changes made in
+ * the current tab shouldn't be reflected to other tabs and to the storage
+ */
 export function isPreview() {
   return !canStore();
 }
