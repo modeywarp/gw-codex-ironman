0124b7bb8bd4b88f55881c4ddb302bde165d2760
Aelto <Aelto@users.noreply.github.com>
2023-07-01 11:15:53 +0200

    #12 wip: need to find design

--- src/App.svelte
+++ src/App.svelte
@@ -120,6 +120,22 @@
           </optgroup>
         {/each}
       </select>
+      <!-- <a
+        href={`https://wiki.guildwars.com${$store_selected_outpost.link}`}
+        target="_blank">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6">
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+        </svg>
+      </a> -->
       <SuggestedOutposts />
     {/if}
 
@@ -182,11 +198,16 @@
     margin: 0;
     padding: 0;
     font-size: 3em;
-    background-color: transparent;
+    background: transparent;
   }
 
-  .outpost-name > * {
+  .outpost-name * {
     font-size: 1rem;
+    background: white;
+  }
+
+  .outpost-name + a {
+    display: inline;
   }
 
   .skillsets {
