5f518c00fe8ae63571be993986cb108ee3351349
Aelto <Aelto@users.noreply.github.com>
2023-07-15 10:29:40 +0200

    #18 add ability to copy template code directly

--- src/components/hero-builds/HeroSkillbar.svelte
+++ src/components/hero-builds/HeroSkillbar.svelte
@@ -26,6 +26,20 @@
 
     notify_info("Build template copied.");
   }
+
+  async function copyBuildTemplateCode() {
+    notify_info("Generating build template code...");
+
+    const response = await encodeBuildTemplate(
+      profession,
+      "none",
+      new Map(build.map((skill, i) => [i, { skill, profession }]))
+    );
+
+    navigator.clipboard.writeText(response.code);
+
+    notify_info("Build template code copied.");
+  }
 </script>
 
 <div class="hero-skillbar">
@@ -33,9 +47,7 @@
     <SkillIcon {skill} {profession} compact={$store_compact_icons} />
   {/each}
 
-  <button
-    on:click={copyBuildTemplate}
-    title="Generate & copy build template code">
+  <button on:click={copyBuildTemplate} title="Generate & copy build template">
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
@@ -49,6 +61,23 @@
         d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
     </svg>
   </button>
+
+  <button
+    on:click={copyBuildTemplateCode}
+    title="Generate & copy build template code">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-6 h-6">
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
+    </svg>
+  </button>
 </div>
 
 <style>
@@ -59,7 +88,7 @@
   }
 
   button {
-    margin: 0 1em;
+    margin-left: 1em;
     box-shadow: 0px 0px 6px rgba(20, 20, 20, 0.15);
   }
 </style>
